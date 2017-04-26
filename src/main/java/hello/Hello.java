32c1cc9ac85fb337010c5e9da2a19696dd3d9256
Björn Eiderbäck <bjorn.eiderback@gmail.com>
2017-04-26 20:32:42 +0200

    And an even more trivial servlet

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,27 @@
+package hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            out.println("Hello!");
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "The most trivial servlet ever...";
+
+    }
+}
