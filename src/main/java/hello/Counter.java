d52008c234cf43aae74c13b5b41a35f6d14a002f
Björn Eiderbäck <bjorn.eiderback@gmail.com>
2017-04-26 20:37:38 +0200

    A servlet with a counter increments each time you call it

---
+++ src/main/java/hello/Counter.java
@@ -0,0 +1,29 @@
+package hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/counter")
+public class Counter extends HttpServlet {
+    int counter = 0;
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            out.println(Integer.valueOf(counter++).toString());
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "The most trivial servlet ever...";
+
+    }
+}
